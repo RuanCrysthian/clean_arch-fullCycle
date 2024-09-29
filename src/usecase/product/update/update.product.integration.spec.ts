047ad758629b73b96df657b44c5ca76fa64b11c8
Ruan Ferraz <ruanlima155@gmail.com>
2024-09-28 22:01:11 -0300

    test: integration test to update product use case

---
+++ src/usecase/product/update/update.product.integration.spec.ts
@@ -0,0 +1,41 @@
+import { Sequelize } from "sequelize-typescript";
+import ProductFactory from "../../../domain/product/factory/product.factory";
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import UpdateProductUseCase from "./update.product.usecase";
+
+describe("Test update product use case", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should update a product", async () => {
+    const productRepository = new ProductRepository();
+    const product = ProductFactory.create("a", "Product", 100);
+    productRepository.create(product);
+    const input = {
+      id: product.id,
+      name: "Product Update",
+      price: 200,
+    };
+    const productUpdateUseCase = new UpdateProductUseCase(productRepository);
+
+    const output = await productUpdateUseCase.execute(input);
+
+    expect(output).toEqual(input);
+  });
+});
